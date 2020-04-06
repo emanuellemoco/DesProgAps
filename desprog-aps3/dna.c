99aabe20d7848ada7eb695778e2e002504b2f485
lucaslealvale <lucaslealvale01@gmail.com>
2020-04-05 23:41:35 -0300

    concerto na matrix int_test pass

--- desprog-aps3/dna.c
+++ desprog-aps3/dna.c
@@ -67,10 +67,10 @@ int mlcs(char a[], int n, char b[], int m)
 
 int dlcs(char a[], int n, char b[], int m)
 {
-  int length[n][m];
-  for (int i = 0; i < n; i++)
+  int length[n+1][m+1];
+  for (int i = 0; i < n+1; i++)
   {
-    for (int j = 0; j < m; j++)
+    for (int j = 0; j < m+1; j++)
     {
       if (i == 0 || j == 0) {
         length[i][j] = 0;
